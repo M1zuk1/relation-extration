411e397d7c8a2e54baceb677fde7b82cf18e113c
M1zuk1 <63490624+M1zuk1@users.noreply.github.com>
2022-04-05 16:24:08 +0800

    Add files via upload
    tokenizer编写完成

---
+++ relation_loader.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： mizuki
+# datetime： 2022/4/5 15:48 
+# ide： PyCharm
+
+import json
+import os
+from config import Config
+
+
+class RelationLoader(object):
+    def __init__(self,config):
+        self.data_dir = config.data_dir
+
+
+    def __load_relation(self):
+        relation_file = os.path.join(self.data_dir, 'relation2id.txt')
+        rel2id = {}
+        id2rel = {}
+        with open(relation_file, 'r', encoding='utf-8') as fr:
+            for line in fr:
+                relation, id_s = line.strip().split()
+                id_d = int(id_s)
+                rel2id[relation] = id_d
+                id2rel[id_d] = relation
+        return rel2id, id2rel, len(rel2id)
+
+    def get_relation(self):
+        return self.__load_relation()
+
+if __name__ == '__main__':
+    config = Config()
+    relationloader = RelationLoader(config)
+    print(relationloader.get_relation())
--- tokenizer.py
+++ tokenizer.py
@@ -4,23 +4,77 @@
 # datetime： 2022/4/4 19:49 
 # ide： PyCharm
 
-import torch
 import os
 import json
-from torch.utils.data import DataLoader,Dataset
+from transformers import BertTokenizer
+from config import Config
+
+
+
 
 class Tokenizer(object):
+    # 初始化tokenize
     def __init__(self, config):
         self.data_dir = config.data_dir
         self.ptlm_dir = config.ptlm_dir
-        self.tokenizer = self.__load_tokenize()
+        self.tokenizer, self.special_tokens = self.load_tokenize()
 
-    def __load_tokenize(self):
-        pass
+    # 加载tokenize
+    def load_tokenize(self):
+        #  加载Bert分词器
+        tokenizer = BertTokenizer.from_pretrained(self.ptlm_dir)
+        # 分词器中添加sepcial——tokens
+        special_tokens = ['<e1>', '</e1>', '<e2>', '</e2>']
+        special_tokens_dict = {'additional_special_tokens': special_tokens}
+        tokenizer.add_special_tokens(special_tokens_dict)
+        return tokenizer, special_tokens
 
-    def __build_vocab(self):
-        pass
+    # 构建数据的词表
+    def build_vocab(self):
+        vocab = set()
+        file_list = ['train', 'test']
+        for file_name in file_list:
+            file_path = os.path.join(self.data_dir, file_name+'.json')
+            print('get the result of tokenization from {}'.format(file_path))
+            with open(file_path, 'r', encoding='utf-8') as read_file:
+                data = json.load(read_file)
+                for i in range(len(data)):
+                   for token in data[i]['sentence']:
+                       if token in self.special_tokens:
+                           continue
+                       vocab.add(token)
+        return vocab
 
 
+
+    # 将vocab中的word切分成subword，然后转化为token_idx
     def get_vocab(self):
-        pass
+        vocab_set = self.build_vocab()
+        vocab_dict = {}
+        extra_tokens = ['[CLS]', '[SEP]', '[PAD]'] + self.special_tokens
+        for token in extra_tokens:
+            vocab_dict[token] = [self.tokenizer.convert_tokens_to_ids(token)]
+
+
+        for token in vocab_set:
+            token = token.lower()
+            if token in vocab_dict.keys():
+                continue
+            # 切分子词
+            token_res = self.tokenizer.tokenize(token)
+            if len(token_res) < 1:
+                token_idx_list = self.tokenizer.convert_tokens_to_ids('[UNK]')
+            else:
+                token_idx_list = self.tokenizer.convert_tokens_to_ids(token_res)
+
+            vocab_dict[token] = token_idx_list
+        return vocab_dict
+
+
+
+
+
+if __name__ == '__main__':
+    config = Config()
+    tokenizer = Tokenizer(config)
+    tokenizer.get_vocab()
